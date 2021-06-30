4edbe63c9f01b4d33c5c5fffde1b0d1a52133cdc
sumit261 <sumitpanchal546@gmail.com>
2021-06-30 20:55:47 +0530

    sling shot complete

--- Slingshot.js
+++ Slingshot.js
@@ -20,13 +20,42 @@ class SlingShot{
 
     display(){ image(this.sling1,200,20);
         image(this.sling2,170,20);
+        
+        
+        
+       
+
+
+    
 
         if(this.Slingshot.bodyA){
             
             var pointA = this.Slingshot.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x < 220) {
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y);
+                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
+                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y);
+                image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
+            }
+
+    
+
+            
+
+
+
+
+
+
+        
+
 
            
             
